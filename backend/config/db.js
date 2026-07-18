9354bd4dae8394625eabd26d00f0b90de2f76a1f
Sparsh Gumasta <sparshgumasta899@gmail.com>
2026-07-18 17:58:07 +0530

    Simplify MongoDB connection error logging
    Removed error details from MongoDB connection error log.

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async()=>{
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected ${conn.connection.host}`);
     }catch(error){
-        console.log("Error Connecting Mongodb",error);
+        console.log("Error Connecting Mongodb");
         process.exit(1);
     }
 }
